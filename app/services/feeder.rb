9eabe63875bc89fd3ece22630bd8b0ffc53c35d3
Justyna Stebel <justyna.stebel@gmail.com>
2015-05-21 20:37:33 +0200

    Prepared layout in sass

--- app/services/feeder.rb
+++ app/services/feeder.rb
@@ -1,6 +1,11 @@
 class Feeder
-  def read_last_messages(n)
-    messages = ContactForm.where(message_read: false).order(:created_at).limit(n)
+  def read_last_messages(n, options = {})
+    messages = ContactForm.where(message_read: false).order(:created_at)
+    messages = messages.where(category: options[:category]) if options[:category]
+    messages = messages.where(email: options[:email]) if options[:email]
+    messages = messages.where(price: options[:price]) if options[:price]
+    messages = messages.limit(n)
+
     messages.update_all(message_read: true)
     messages.to_a
   end
